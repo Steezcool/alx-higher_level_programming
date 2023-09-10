fac7f3b5c10275d3221b051153cb1bf97464ddcd
Steezcool <petermcmillan25@gmail.com>
2023-09-10 00:25:25 -0700

    update4

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+# 6-print_matrix_integer.py
+
+
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for col in row:
-            print("{:d}".format(col), end=" " if col != row[-1] else "")
-        print()#!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
+    """prints a matrix of integers"""
+
     for row in matrix:
         for col in row:
             print("{:d}".format(col), end=" " if col != row[-1] else "")
