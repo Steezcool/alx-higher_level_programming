58e6e928b5c7b66d0cb33429f9dabdf63b12ecfd
Steezcool <petermcmillan25@gmail.com>
2023-09-25 02:13:16 -0700

    upadate2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
 
+def lenn(my_list=[]):
+    count = 0
+    for i in my_list:
+        count += 1
+    return count
+
+
 def safe_print_list(my_list=[], x=0):
-    total = 0
-    for i in range(x):
-        try:
-            print(f"{my_list[i]}", end="")
-            total += 1
-        except IndexError:
-            break
-    print()
-    return(total)
+    count = 0
+    try:
+        if x > lenn(my_list):
+            raise IndexError
+
+        for i in my_list[0:x]:
+            print(i, end="")
+        return x
+    except IndexError:
+        for i in my_list[0:lenn(my_list)]:
+            print(i, end="")
+        return lenn(my_list)
+    finally:
+        print()
