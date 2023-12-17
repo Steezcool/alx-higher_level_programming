64bdcf10a49a1093e29da553f00088a67cb91bff
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2023-12-17 10:20:31 +0200

    Update

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""lists all State objects from the database hbtn_0e_6_usa"""
+"""List all states"""
 from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.get(State, 2)
-    state.name = "New Mexico"
-    session.add(state)
+    states = session.query(State).filter(State.id == 2).all()
+    if states:
+        states[0].name = "New Mexico"
     session.commit()
-
+    session.close()
