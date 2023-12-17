58ada6d85714ea529c84f3acdbdb74ad425d336d
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2023-12-17 10:01:57 +0200

    Update

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
-"""Task 0 Get all states from  states from the database hbtn_0e_0_usa"""
+"""Lists states"""
+
 import MySQLdb
 from sys import argv
 
-
 if __name__ == "__main__":
-    mysql_username, mysql_password, mysql_db_name = argv[1], argv[2], argv[3]
-
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=mysql_db_name
-    )
-    cursor = db.cursor()
-    query = """
-    SELECT cities.id, cities.name, states.name FROM cities
-    INNER JOIN states
-    ON cities.state_id = states.id
-    ORDER BY cities.id ASC"""
-    all_states = cursor.execute(query)
-
-    for row in cursor.fetchall():
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
+                "JOIN states ON cities.state_id = states.id "
+                "ORDER BY cities.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
-
+    cur.close()
+    conn.close()
