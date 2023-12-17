a6bb251a4e6d26bef5694744ee7cdf46fbc31d56
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2023-12-17 10:27:08 +0200

    Update

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-"""lists all State objects from the database hbtn_0e_100_usa"""
+"""List all states"""
 from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, relationship
 from relationship_state import Base, State
-from relationship_city import Base, City
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    city = City(name="San Francisco")
     new_state = State(name="California")
-    new_state.cities.append(city)
     session.add(new_state)
     session.commit()
+    new_city = City(name="San Francisco", state_id=new_state.id)
+    session.add(new_city)
+    session.commit()
     session.close()
-
