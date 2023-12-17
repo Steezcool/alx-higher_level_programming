0aad47df1a3fd38e46c2c89929c9cdcc09f92db9
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2023-12-17 10:04:23 +0200

    Update

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-"""Task 0 Get all states from  states from the database hbtn_0e_0_usa"""
+"""Lists all states"""
+
 import MySQLdb
 from sys import argv
 
-
 if __name__ == "__main__":
-    mysql_username, mysql_password, mysql_db_name = argv[1], argv[2], argv[3]
-
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=mysql_db_name
-    )
-    cursor = db.cursor()
-    query = """
-    SELECT name FROM cities
-    WHERE cities.state_id = (SELECT id FROM states WHERE name = %s)
-    ORDER BY cities.id ASC"""
-    all_states = cursor.execute(query, (argv[4],))
-    flag = -1
-
-    for row in cursor.fetchall():
-        print(("" if flag == -1 else ", ") + row[0], end="")
-        flag = 1
-    print("")
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
-
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT cities.name FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+        """, (argv[4], ))
+    print(", ".join(map(lambda x: x[0], cur.fetchall())))
+    cur.close()
+    conn.close()
